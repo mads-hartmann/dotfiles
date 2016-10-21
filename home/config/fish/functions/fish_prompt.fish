ccdd2ac95b12149c9c9541a1c33694f00aad256c
Mads Hartmann <mads379@gmail.com>
2016-10-21 09:52:17 +0200

    More resilient prompt

--- home/config/fish/functions/fish_prompt.fish
+++ home/config/fish/functions/fish_prompt.fish
@@ -1,15 +1,20 @@
 function fish_prompt --description 'My preffered prompt'
-        # TODO: This only works in the root dir.
-        if [ -d .git ]
+
+        git rev-parse --is-inside-work-tree > /dev/null ^ /dev/null
+
+        if [ $status != 0 ]
+                set git_info ""
+        else
                 set -l git_branch (git rev-parse --abbrev-ref HEAD)
-                set git_info " [$git_branch]"
+                set git_info " $git_branch"
         end
 
         set path (echo $PWD | sed -e "s|^$HOME|~|")
 
-        set_color purple
+        set_color cyan
         printf "%s" $path
-        set_color normal
+        set_color green
         printf "%s" $git_info
-        printf " \$ "
+        set_color normal
+        printf " "
 end
