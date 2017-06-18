26b53b49066d7d0b761b22aea4316e1081f50107
Mads Hartmann <mads379@gmail.com>
2017-06-18 13:54:33 +0200

    A bit of cleanup

--- home/zsh/plugins/aws-switch/aws-switch.plugin.zsh
+++ home/zsh/plugins/aws-switch/aws-switch.plugin.zsh
@@ -1,7 +1,10 @@
 function aws-switch() {
     case ${1} in
+        "")
+            unset AWS_PROFILE
+            ;;
         clear)
-            export AWS_PROFILE=""
+            unset AWS_PROFILE
             ;;
         *)
             export AWS_PROFILE="${1}"
