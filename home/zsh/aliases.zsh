ea4bd0d186bfe2cd17cbd5143d28e678ab1c7cc5
Mads Hartmann <mads379@gmail.com>
2017-07-10 13:59:03 +0200

    Never again bother me with that tiny typo

--- home/zsh/aliases.zsh
+++ home/zsh/aliases.zsh
@@ -3,10 +3,11 @@
 #    various commands that I'm too lazy to type out all the time.
 
 alias reload=src
-alias f="famlydev"
+alias f=famlydev
 alias epoch="date +%s"
 alias e="emacsclient -nw --socket-name=shell-emacs"
 alias eg="/Applications/Emacs.app/Contents/MacOS/bin/emacsclient -c --no-wait"
+alias gti=git # DWIM version of git.
 
 # Starts an emacs daemon with a dedicated name. I use this daemon
 # whenever I use emacs from the terminal.
