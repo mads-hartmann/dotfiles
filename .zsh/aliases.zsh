ebbe90f5c6358ba5ed4ecaca6307fbefb548121b
Mads Hartmann <mads379@gmail.com>
2018-01-02 11:13:03 +0100

    I always want new GUI frames

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -6,7 +6,7 @@ alias reload=src
 alias f=famlydev
 alias epoch="date +%s"
 alias e="emacsclient -nw --socket-name=shell-emacs"
-alias eg="emacsclient --no-wait --socket-name server-gui"
+alias eg="emacsclient -c --no-wait --socket-name server-gui"
 alias gti=git # DWIM version of git.
 
 alias dps="docker ps --format 'table {{.ID}}\t{{.Names}}\t{{.Ports}}\t{{.Status}}'"
