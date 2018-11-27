d784b9ae72c00bd0cef31b1c0a90268ef464e08e
Mads Hartmann <mads379@gmail.com>
2018-11-27 22:10:37 +0100

    Remove zsh config that I've moved to org-mode

--- .zsh/plugins/hello/hello.plugin.zsh
+++
@@ -1,8 +0,0 @@
-# Useless command. I wrote this as part of this blog post about writing
-# zsh completion scripts
-#
-#  http://mads-hartmann.com/2017/08/06/writing-zsh-completion-scripts.html
-#
-function hello {
-    echo "$@"
-}
--- .zsh/plugins/theme/theme.plugin.zsh
+++
@@ -1,9 +0,0 @@
-# https://coderwall.com/p/s-2_nw/change-iterm2-color-profile-from-the-cli
-
-function theme() {
-    echo -e "\033]50;SetProfile=$1\a"
-
-    # This is to make sure that my danger-danger plugin doesn't immediately
-    # switch the profile back to 'Default' on each pre-command hook.
-    export ITERM_THEME=$1
-}
